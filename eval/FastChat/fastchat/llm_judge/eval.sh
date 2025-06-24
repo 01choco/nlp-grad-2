93ce2999b4031e40e8dc614bd2868f86d684e9fd
이서형 <ruth1701@sogang.ac.kr>
2025-06-24 23:27:23 +0900

    add fastchat code

---
+++ eval/FastChat/fastchat/llm_judge/eval.sh
@@ -0,0 +1,4 @@
+python gen_judgment_noenter.py --judge-model gpt-4o-mini --model-list llama3-8b-sft --parallel 2 --bench-name evol_instruct 
+python gen_judgment_noenter.py --judge-model gpt-4o-mini --model-list llama3-8b-sft --mode pairwise-baseline --parallel 2 --bench-name evol_instruct
+
+
